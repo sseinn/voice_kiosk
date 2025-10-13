3cb63d07a8c779caa3018c910c17f6949bf5a2ef
sseinn <uoeo21@jnu.ac.kr>
2025-10-14 02:20:15 +0900

    feat: Header 컴포넌트 구현

---
+++ voice_kiosk/src/components/Header.tsx
@@ -0,0 +1,9 @@
+export default function Header() {
+  return (
+    <div className="h-48 px-96 inline-flex justify-center items-center gap-2.5 overflow-hidden">
+      <div className="justify-start text-black text-7xl font-normal font-['Tmoney_RoundWind']">
+        팬도로시
+      </div>
+    </div>
+  );
+}
