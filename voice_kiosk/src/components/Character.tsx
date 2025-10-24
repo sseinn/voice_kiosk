4de34b080c2f352fd80baa03f819e47c1e7c32dd
sseinn <uoeo21@jnu.ac.kr>
2025-10-25 03:15:02 +0900

    fix: Character 위치 조정

--- voice_kiosk/src/components/Character.tsx
+++ voice_kiosk/src/components/Character.tsx
@@ -2,10 +2,20 @@ import characterImg from "../assets/Character.png";
 
 export default function Character() {
   return (
+<<<<<<< Updated upstream
     <img
       src={characterImg}
       alt="직원 캐릭터"
       className="w-[625.5px] h-[674px] object-contain"
     />
+=======
+    <div className="fixed left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 z-10">
+      <img
+        src={characterImg}
+        alt="직원 캐릭터"
+        className="w-[625.5px] h-[674px] object-contain"
+      />
+    </div>
+>>>>>>> Stashed changes
   );
 }
